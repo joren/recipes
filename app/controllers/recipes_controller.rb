9b727444d026b4b41bde001b8fdec3d5f04775d7
Joren De Groof <git@jorendegroof.be>
2015-03-21 17:27:09 +0100

    Adds edit recipe view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,6 +20,17 @@ class RecipesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render :edit
+    end
+  end
+
 private
 
   def load_recipe
