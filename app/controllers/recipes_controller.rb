d1217ae53e9b7ce60406db7384a4a4e3cf6a82c4
Joren De Groof <git@jorendegroof.be>
2015-03-21 15:56:12 +0100

    Adds recipe detail

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,15 @@
 class RecipesController < ApplicationController
+  before_action :load_recipe, only: [:show, :edit, :update, :destroy]
   def index
     @recipes = Recipe.all
+  end
+
+  def show
+  end
+
+private
+
+  def load_recipe
+    @recipe = Recipe.find(params[:id])
   end
 end
