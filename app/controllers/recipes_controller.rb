c4980b786e706fc90ef4fac782027c2c3775c2a2
Joren De Groof <git@jorendegroof.be>
2015-03-21 17:20:06 +0100

    Adds new recipe form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,9 +7,31 @@ class RecipesController < ApplicationController
   def show
   end
 
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new
+    end
+  end
+
 private
 
   def load_recipe
     @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :name,
+      :people,
+      :duration,
+      :instructions
+    )
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,6 @@
 class Recipe < ActiveRecord::Base
+  validates :name, presence: true
+  validates :people, presence: true
+  validates :duration, presence: true
+  validates :instructions, presence: true
 end
