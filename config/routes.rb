3ef3bf285c556d42124079ba6d7c5ec0215a94eb
Joren De Groof <git@jorendegroof.be>
2015-04-25 15:14:45 +0200

    Adds sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'recipes#index'
+  get "logout" => "sessions#destroy", :as => "logout"
+  get "login" => "sessions#new", :as => "login"
 
   resources :recipes
+  resources :sessions, only: :create
 end
